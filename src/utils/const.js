934472a9d729b41cd132a9f6aaebb223b3a5959e
digmen <digmen68@gmail.com>
2024-02-27 15:28:40 +0000

    change_api

--- src/utils/const.js
+++ src/utils/const.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://api.valantis.store:40000';
+export const BASE_URL = 'https://api.valantis.store:41000/';
 
 export const ACTIONS = {
     productList: 'productsList',
